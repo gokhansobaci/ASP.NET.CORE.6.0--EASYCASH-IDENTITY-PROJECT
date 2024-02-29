d996d00843b438f3040f0203ac1102e947e1031e
gokhansobaci <gokhansobaci9@gmail.com>
2024-02-29 11:59:57 +0300

    ASP.NET.CORE.6.0--EASYCASH-IDENTİTY-PROJECT
    Asp.Net Core 6.0 ile geliştirilen üyelik sistem temelli banka işlemlerini kolaylıkla halledebileceğiniz bir kuruluş arayüzü

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,39 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-R7AR1ND;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+        public DbSet<ElectricBill> ElectricBills { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.SenderCustomer)
+                .WithMany(y => y.CustomerSender)
+                .HasForeignKey(z => z.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.ReceiverCustomer)
+                .WithMany(y => y.CustomerReceiver)
+                .HasForeignKey(z => z.ReceiverID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
@@ -0,0 +1,23 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DataAccessLayer.Repositories;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.EntityFramework
+{
+    public class EfCustomerAccountProcessDal : GenericRepository<CustomerAccountProcess>, ICustomerAccountProcessDal
+    {
+        public List<CustomerAccountProcess> MyLastProcess(int id)
+        {
+            using var context = new Context();
+            var values = context.CustomerAccountProcesses.Include(y => y.SenderCustomer).Include(w=>w.ReceiverCustomer).ThenInclude(z => z.AppUser).Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
+            return values;
+        }
+    }
+}
